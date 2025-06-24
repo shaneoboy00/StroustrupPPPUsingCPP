14e5790c24784a9802185a04d80b029a4452d362
Misa Lisa <mrsomethingor4no7h3r@protonmail.com>
2025-06-24 12:35:42 -0400

    fix: added some permanent and temporary files

---
+++ Chapter2/Exercises/hello_world/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "../../../std_lib_facilities.h"
+int main()    // C++ programs start by executing the function main
+{
+    cout << "Hello, programming!\n";    // output "Hello, programming!"
+    cout << "Here we go!\n";    // output "Here we go!"
+    return 0;
+}
--- Chapter2/Section5/Drills/Projects/hello_world/hello_world.cpp
+++ Chapter2/Section5/Drills/Projects/hello_world/hello_world.cpp
@@ -1,4 +1,4 @@
-#include "../../../std_lib_facilities.h"
+#include "../../../../../std_lib_facilities.h"
 int main()    // C++ programs start by executing the function main
 {
     cout << "Hello, World!\n";    // output "Hello, World!"
